e42f6b6e87f58037272debd2fa88adccc6ab4e84
Rasnita Sitoula <iamrasnita@gmail.com>
2025-01-28 15:54:01 +0000

    changed the src path

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,8 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-    <img src="../../../../images/SH_Logo_B.png" alt="image" class="mx-auto" style="width:235px"/>
+    <img src="{{ asset('images/SH_Logo_B.png')}}" alt="image" class="mx-auto" style="width:235px"/>
+    {{-- <img src="{{ asset('images/SH_Logo_B.png')}}" alt="image" class="mx-auto" style="width:235px"/> --}}
 </a>
 </td>
 </tr>
